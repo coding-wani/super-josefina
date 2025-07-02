ddcb3d345d4f618b7a896c5fd75923397775e9e1
Coding Wani <79173984+coding-wani@users.noreply.github.com>
2025-07-02 18:37:31 +0200

    feat: wrap Home main area content in a div for improved styling consistency

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
 import { MainArea } from "@/components/layout/Content";
 
 export default function Home() {
-  return <MainArea pageTitle="Home">Home main area content</MainArea>;
+  return (
+    <MainArea pageTitle="Home">
+      <div className="px-3 py-1">Home main area content</div>
+    </MainArea>
+  );
 }
